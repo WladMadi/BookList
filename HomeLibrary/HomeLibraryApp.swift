0f55780693d11e598a6076f86fb53490afb6cc65
Vlad Madi <wlad@madi.space>
2025-10-03 20:10:07 +0300

    Initial Commit

---
+++ HomeLibrary/HomeLibraryApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomeLibraryApp.swift
+//  HomeLibrary
+//
+//  Created by Влад Мади on 03.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct HomeLibraryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
